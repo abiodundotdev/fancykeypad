7f90a7a00f38645b28b11acfc7511e52bc708734
Qazeem Abiodun <qudusqazeem@gmail.com>
2023-09-22 17:24:32 +0100

    upated test

--- test/fancykeypad_test.dart
+++ test/fancykeypad_test.dart
@@ -7,15 +7,65 @@ void main() {
   late Widget keypadWidget;
 
   setUpAll(() {
+    final notifier = ValueNotifier<String>("");
     keypadWidget = MaterialApp(
-      home: Material(child: FancyKeypad(onKeyTap: (va) {}, maxLength: 4)),
+      home: Material(
+        child: Column(children: [
+          Expanded(
+            flex: 2,
+            child: ValueListenableBuilder(
+              valueListenable: notifier,
+              builder: (_, String val, __) {
+                return Text(val);
+              },
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: FancyKeypad(
+                onKeyTap: (va) {
+                  notifier.value = va;
+                },
+                maxLength: 4,
+              ),
+            ),
+          )
+        ]),
+      ),
     );
   });
 
   group("Keypad test", () {
-    testWidgets("Vefiry has required ", (widgetTester) async {
+    testWidgets("Verify has required number of keypad", (widgetTester) async {
       await widgetTester.pumpWidget(keypadWidget);
-      //final finder  =
+      await widgetTester.pumpAndSettle();
+      final keyPads = find.byType(FancyKeypadButton);
+      expect(keyPads, findsNWidgets(6));
+    });
+
+    testWidgets("Verify correct text are returned when key is pressed",
+        (widgetTester) async {
+      String expectedtext = "";
+      await widgetTester.pumpWidget(keypadWidget);
+      await widgetTester.pumpAndSettle();
+      final oneKey = find.text("1");
+      expect(oneKey, findsOneWidget);
+      final twoKey = find.text("2");
+      expect(twoKey, findsOneWidget);
+      final threeKey = find.text("3");
+      expect(threeKey, findsOneWidget);
+      await widgetTester.tap(oneKey);
+      await widgetTester.pumpAndSettle(Duration(seconds: 2));
+      expectedtext += "1";
+      await widgetTester.tap(twoKey);
+      await widgetTester.pumpAndSettle(Duration(seconds: 2));
+      expectedtext += "2";
+      await widgetTester.tap(threeKey);
+      await widgetTester.pumpAndSettle(Duration(seconds: 2));
+      expectedtext += "3";
+      expect(find.text(expectedtext), findsOneWidget);
     });
   });
 }
